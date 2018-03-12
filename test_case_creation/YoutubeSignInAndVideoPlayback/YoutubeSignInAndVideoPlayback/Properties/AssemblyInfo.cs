b353901ffb4f932b04a09a98c72477ae1124ded4
iartyushin <smellmemory55@gmail.com>
2018-03-12 16:40:53 +0300

    first test case creation
    youtube:
    - sign in
    - find a particular video
    - sign out

---
+++ test_case_creation/YoutubeSignInAndVideoPlayback/YoutubeSignInAndVideoPlayback/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("YoutubeSignInAndVideoPlayback")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("YoutubeSignInAndVideoPlayback")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c616b366-2997-4930-ba56-06e29d182571")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test_case_creation/YoutubeSignInAndVideoPlayback/YoutubeSignInAndVideoPlayback/YoutubeSmokeTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace YoutubeSmokeTest
+{
+    public class YoutubeSmokeTest
+    {
+
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void YoutubeSignInAndVideoPlaybackTest1()
+        {
+            driver.Navigate().GoToUrl("https://youtube.com");
+            IWebElement signInButton = driver.FindElement(By.Id("text"));
+            signInButton.Click();
+
+            //IWebElement useAnotherAccount = driver.FindElement(By.CssSelector("div + p[text='Use another account']"));
+            //useAnotherAccount.Click();
+
+            IWebElement enterEmailField = driver.FindElement(By.CssSelector("div > input[type='email']"));
+            enterEmailField.SendKeys("mattaku.ad@gmail.com");
+
+            IWebElement nextButton = driver.FindElement(By.CssSelector("div#identifierNext > content > span"));
+            nextButton.Click();
+
+            IWebElement enterPasswordField = driver.FindElement(By.CssSelector("div > input[type='password']"));
+            enterPasswordField.SendKeys("Q!w2e3r4");
+
+            IWebElement doneButton = driver.FindElement(By.CssSelector("div#passwordNext > content > span"));
+            doneButton.Click();
+
+            IWebElement searchField = driver.FindElement(By.CssSelector("#search"));
+            searchField.SendKeys("gnossienne 1 piano");
+            searchField.SendKeys(Keys.Enter);
+
+            IWebElement firstVideoInGrid = driver.FindElement(By.XPath(".//a[@title='ERIK SATIE Gnossienne 1 - Alessio Nanni, piano']"));
+            firstVideoInGrid.Click();
+
+            //var firstHeading = driver.FindElement(By.CssSelector("div#container > h1 > yt-formatted-string")).Text;
+            //NUnit.Framework.Assert.AreEqual("ERIK SATIE Gnossienne 1 - Alessio Nanni, piano", firstHeading);
+
+
+            Thread.Sleep(5000);
+
+            var pageTitle = driver.Title;
+            NUnit.Framework.StringAssert.Contains("ERIK SATIE Gnossienne 1 - Alessio Nanni, piano", pageTitle);
+
+            IWebElement avatarButton = driver.FindElement(By.CssSelector("button#avatar-btn"));
+            avatarButton.Click();
+
+            IWebElement signOutButton = driver.FindElement(By.XPath("//a[@href=\"/logout\"]"));
+            signOutButton.Click();
+
+        }
+
+
+        
+    }
+}
